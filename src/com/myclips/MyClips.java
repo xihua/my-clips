2353e3852e00a5eca6bb94f0fe4de556edc5d569
hckirmizi <hckirmizi@67c44b69-57a6-fdac-a869-a6ee374a5b07>
2010-05-25 00:12:17 +0000

    Flipping over different content lists is done in MyClips.java. \\TODO: Replace the DB queries with list views inside method 'OnTouch()' in MyClips.java

--- src/com/myclips/MyClips.java
+++ src/com/myclips/MyClips.java
@@ -1,46 +1,81 @@
 package com.myclips;
 
 import android.app.Activity;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnTouchListener;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
 
-public class MyClips extends Activity {
-	
-	public static final String TAG = "ContactManager";
-	private ClipboardDbManager mDbHelper;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.clipboard_list);
-    	mDbHelper = new ClipboardDbAdapter(this);
-    	mDbHelper.open();
-    	fillData();
-    }
-    
-    public void fillData() {
-    	Cursor c = mDbHelper.fetchAllCliboards();
-    	startManagingCursor(c);
-    	
-    	// TODO: Here we have to learn how to output all clipboards as we can
-    	// slide left/right on touch screen to change the clipboard
-    	// Now, this just returns a list view
-    	String[] from = new String[] { ClipboardDbAdapter.KEY_TITLE };
-    	int[] to = new int[] { /* TODO: R.id.text1 */};
-    	
-    	// Now create an array adapter and set it to display using our row
-        SimpleCursorAdapter clipboards =
-            new SimpleCursorAdapter(this, R.layout.clipboards_row, c, from, to);
-        setListAdapter(clipboards);
-    }
+public class MyClips extends Activity implements OnTouchListener {
 
-    
+	// TODO: Clean this Test Case.
+	static String[] items = { "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
+			"adipiscing", "elit", "morbi", "vel", "ligula", "vitae", "arcu", "aliquet", "mollis",
+			"etiam", "vel", "erat", "placerat", "ante", "porttitor", "sodales", "pellentesque",
+			"augue", "purus" };
+
+	// private ClipboardDbManager mDbHelper;
+	private float downXValue;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// mDbHelper = new ClipboardDbAdapter(this);
+		// mDbHelper.open();
+		// fillData();
+		LinearLayout ll = (LinearLayout) findViewById(R.id.layout_main);
+		ll.setOnTouchListener((OnTouchListener) this);
+
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent me) {
+		// upXValue: X coordinate when the user releases the finger
+		float upXValue = 0;
+		
+		// reference to the ViewFlipper
+		ViewFlipper vf = (ViewFlipper) findViewById(R.id.details);
+		
+		// TODO: Here we have to replace this loop elements with the clipboards
+		for (String item : items) {
+			Button btn = new Button(this);
+			btn.setText(item);
+			vf.addView(btn, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+					ViewGroup.LayoutParams.WRAP_CONTENT));
+		}
+
+		switch (me.getAction()) {
+			case MotionEvent.ACTION_DOWN: {
+				downXValue = me.getX();
+				break;
+			}
+			case MotionEvent.ACTION_UP: {
+				upXValue = me.getX();
+				// finger moving toward left
+				if (downXValue < upXValue) {
+					// set animation
+					vf.setAnimation(AnimationUtils.loadAnimation(this, R.anim.push_left_out));				
+					// flip
+					vf.showPrevious();
+				}
+				// finger moving toward right
+				if (downXValue > upXValue) {
+					//ViewFlipper vf = (ViewFlipper) findViewById(R.id.details);
+					vf.setAnimation(AnimationUtils.loadAnimation(this, R.anim.push_left_in));
+					vf.showNext();
+				}
+				break;
+			}
+		}
+
+		// if return false, these actions won't be recorded
+		return true;
+	}
+
 }
