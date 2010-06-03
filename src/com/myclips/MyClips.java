cfec325014aac1587716beda17f24b062390e158
hckirmizi <hckirmizi@67c44b69-57a6-fdac-a869-a6ee374a5b07>
2010-06-03 09:50:34 +0000

    listview test is done.

--- src/com/myclips/MyClips.java
+++ src/com/myclips/MyClips.java
@@ -1,12 +1,19 @@
 package com.myclips;
 
+import com.myclips.db.Clip;
+import com.myclips.db.ClipboardDbAdapter;
+import com.myclips.prefs.AppPrefs;
 import com.myclips.service.ClipboardMonitor;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,30 +21,53 @@ import android.view.View.OnTouchListener;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ResourceCursorAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.ViewFlipper;
 
-public class MyClips extends Activity implements OnTouchListener, LogTag {
-    //private final static String TAG = "MyClips"; 
-
-	// TODO: Clean this Test Case.
-	static String[] items = { "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
-			"adipiscing", "elit", "morbi", "vel", "ligula", "vitae", "arcu", "aliquet", "mollis",
-			"etiam", "vel", "erat", "placerat", "ante", "porttitor", "sodales", "pellentesque",
-			"augue", "purus" };
-
-	// private ClipboardDbManager mDbHelper;
-	private float downXValue;
-
+//public class MyClips extends Activity implements OnTouchListener, LogTag {
+public class MyClips extends ListActivity implements LogTag {
+	
+	private ClipboardDbAdapter mDbHelper;
+	private float downXValue;	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		// mDbHelper = new ClipboardDbAdapter(this);
-		// mDbHelper.open();
-		// fillData();
-		LinearLayout ll = (LinearLayout) findViewById(R.id.layout_main);
-		ll.setOnTouchListener((OnTouchListener) this);
+		getListView().setOnCreateContextMenuListener(this);
+		mDbHelper = new ClipboardDbAdapter(this);
+		
+		getClips();
+		
+		//LinearLayout ll = (LinearLayout) findViewById(R.id.clipList);
+		//ll.setOnTouchListener((OnTouchListener) this);
 		startClipboardMonitor();
+		
+	}
+	
+	private void getClips() {
+		Cursor clipsCursor = mDbHelper.queryAllClips(new String[] { Clip._ID, Clip.COL_DATA }, 1);
+		startManagingCursor(clipsCursor);
+		
+		Log.i(TAG, "clip count = " + clipsCursor.getCount());
+		
+		String[] from = new String[] { Clip.COL_DATA };
+		int[] to = new int[] { R.id.clipEntryText };
+		
+		SimpleCursorAdapter mAdapter = 
+			new SimpleCursorAdapter(this, R.layout.clip_entry, clipsCursor, from, to);
+		
+
+		Log.i(TAG, "mAdapter = " + mAdapter);
+		
+		setListAdapter(mAdapter);
+		
+		//ViewFlipper vf = (ViewFlipper) findViewById(R.id.details);		
+		//vf.addView(lv, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+		//		ViewGroup.LayoutParams.WRAP_CONTENT));
+		
 	}
 
 	/* When ClipboardMonitor doesn't start on boot due to the reason like we
@@ -53,7 +83,16 @@ public class MyClips extends Activity implements OnTouchListener, LogTag {
                     + ClipboardMonitor.class.getName());
         }
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    super.onCreateOptionsMenu(menu);
+	    menu.add(0, INSERT_ID, 0, R.string.create_new_clipboard);
+	    inflater.inflate(R.menu.menu, menu);
+	    return true;
+	}
 
+	/*
 	@Override
 	public boolean onTouch(View v, MotionEvent me) {
 		// upXValue: X coordinate when the user releases the finger
@@ -69,7 +108,7 @@ public class MyClips extends Activity implements OnTouchListener, LogTag {
 			vf.addView(btn, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
 					ViewGroup.LayoutParams.WRAP_CONTENT));
 		}
-
+		
 		switch (me.getAction()) {
 			case MotionEvent.ACTION_DOWN: {
 				downXValue = me.getX();
@@ -97,5 +136,6 @@ public class MyClips extends Activity implements OnTouchListener, LogTag {
 		// if return false, these actions won't be recorded
 		return true;
 	}
-
+	*/
+	
 }
